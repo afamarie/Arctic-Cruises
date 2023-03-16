66c2c0080297eca6e6272b83b0ef568a035682db
afamaria <marie.afanasyeva@gmail.com>
2023-03-16 13:19:37 +0300

    Fix after code review v1.2

--- source/js/modules/burger.js
+++ source/js/modules/burger.js
@@ -3,7 +3,6 @@ const header = document.querySelector('.header');
 const body = document.querySelector('body');
 const links = header.querySelectorAll('a');
 const overlay = header.querySelector('.header__overlay');
-const intro = header.querySelector('.container--intro-content');
 
 header.classList.remove('header--no-js');
 
@@ -11,17 +10,14 @@ const onClickToggleMenu = () => {
   header.classList.toggle('header--menu-opened');
   if (body.style.overflow === 'hidden') {
     body.style.overflow = '';
-    intro.style.marginTop = '';
   } else {
     body.style.overflow = 'hidden';
-    intro.style.marginTop = '68px';
   }
 };
 
 const onClickCloseMenu = () => {
   header.classList.remove('header--menu-opened');
   body.style.overflow = '';
-  intro.style.marginTop = '';
 };
 
 button.addEventListener('click', onClickToggleMenu);
